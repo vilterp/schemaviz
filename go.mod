ff186a3cd43fde7a9bdbc2e5f5bfd75ac3eec5aa
Pete Vilter <pete.vilter@gmail.com>
2019-04-02 23:41:12 -0400

    get viz working

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/vilterp/schemaviz
+
+require (
+	github.com/lib/pq v1.0.0
+	github.com/xo/xo v0.0.0-20190105221319-1a94fa516029
+)
--- main.go
+++ main.go
@@ -1,7 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/xo/xo/loaders"
+	"github.com/xo/xo/models"
+)
 
 func main() {
-	fmt.Println("hello world")
+	connString := os.Args[1]
+
+	conn, err := sql.Open("postgres", connString)
+	if err != nil {
+		log.Fatal("connecting to db: ", err)
+	}
+	schema, err := loadSchema(conn)
+	if err != nil {
+		log.Fatal("loading schema: ", err)
+	}
+
+	printDot(schema)
+}
+
+type tableWithFKs struct {
+	table *models.Table
+	fks   []*models.ForeignKey
+}
+
+type schema []*tableWithFKs
+
+func loadSchema(conn *sql.DB) ([]*tableWithFKs, error) {
+	var out schema
+	tables, err := loaders.PgTables(conn, "public", "r")
+	if err != nil {
+		return nil, err
+	}
+	for _, table := range tables {
+		foreignKeys, err := models.PgTableForeignKeys(conn, "public", table.TableName)
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, &tableWithFKs{
+			table: table,
+			fks:   foreignKeys,
+		})
+	}
+	return out, nil
+}
+
+func printDot(schema []*tableWithFKs) {
+	fmt.Println("digraph schema {")
+
+	fmt.Println("splines=ortho")
+	fmt.Println("nodesep=0.4")
+	fmt.Println("ranksep=0.8")
+	fmt.Println(`node [shape="box",style="rounded,filled"]`)
+	fmt.Println(`edge [arrowsize="0.5"]`)
+
+	for _, table := range schema {
+		fmt.Printf("\t\"%s\" [color=\"paleturquoise\"];\n", table.table.TableName)
+
+		for _, fk := range table.fks {
+			fmt.Printf(
+				"\t\"%s\" -> \"%s\" [xlabel=\"%s\"];\n",
+				table.table.TableName, fk.RefTableName, fk.ColumnName,
+			)
+		}
+	}
+
+	fmt.Println("}")
 }
